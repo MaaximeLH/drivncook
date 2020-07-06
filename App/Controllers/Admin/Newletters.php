bfc1dff01e9d5334b0d73634515c174614146b3a
MaaximeLH <lehenaff.maxime@yahoo.Fr>
2020-07-07 01:51:33 +0200

    Algorithme de newletters pour les filtres

--- App/Controllers/Admin/Newletters.php
+++ App/Controllers/Admin/Newletters.php
@@ -4,6 +4,7 @@ namespace App\Controllers\Admin;
 
 use App\Entity\Admin;
 use App\Entity\Customer;
+use App\Entity\Orders;
 use App\Entity\Users;
 use Core\Controller;
 use Core\CSRF;
@@ -43,12 +44,39 @@ class Newletters extends Controller {
                 return View::render('Admin/newletters', ['admin' => $this->admin, 'page' => 'newletters', 'error' => true, 'params' => $params]);
             }
 
+            $usersMinCommandFilter = (isset($params['users_min_commands'])) ? trim(intval($params['users_min_commands'])) : 0;
+            $customersMinCommandFilter = (isset($params['customers_min_commands'])) ? trim(intval($params['customers_min_commands'])) : 0;
+
             $em = Entity::getEntityManager();
             $usersRepository = $em->getRepository(Users::class);
+            $ordersRepository = $em->getRepository(Orders::class);
             $customersRepository = $em->getRepository(Customer::class);
 
-            //TODO: SI $usersFilter ; foreach des users ; récupération des emails si les filtres sont OK
-            // TODO: SI $customersFilter ; foreach des customers ; récupération des emails si les filtres sont OK
+            $emails = [];
+
+            if($usersFilter) {
+                foreach ($usersRepository->findAll() as $user) {
+                    $total = count($ordersRepository->findByUser($user));
+                    if($total > $usersMinCommandFilter) {
+                        $emails[] = $user->getEmail();
+                    }
+                }
+            }
+
+            if($customersFilter) {
+                foreach ($customersRepository->findAll() as $customer) {
+                    $total = count($ordersRepository->findByCustomer($customer));
+                    if($total > $customersMinCommandFilter) {
+                        $emails[] = $customer->getEmail();
+                    }
+                }
+            }
+
+            $emails = implode(', ', $emails);
+
+            //TODO: Envoie de l'email avec le contenu etc...
+            return View::render('Admin/newletters', ['admin' => $this->admin, 'page' => 'newletters', 'success' => true, 'params' => $params]);
+
         }
 
         return View::render('Admin/newletters', ['admin' => $this->admin, 'page' => 'newletters']);
