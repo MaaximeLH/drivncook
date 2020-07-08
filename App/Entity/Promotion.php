7b348fdf4e4d456a6793b0d7d45831b9aee81a5d
MaaximeLH <lehenaff.maxime@yahoo.Fr>
2020-07-08 11:25:51 +0200

    Modification des Entités gérant les promotions, adaptation avec le nouveau système

--- App/Entity/Promotion.php
+++ App/Entity/Promotion.php
@@ -1,5 +1,7 @@
 <?php
+
 namespace App\Entity;
+
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -7,6 +9,7 @@ use Doctrine\ORM\Mapping as ORM;
  *
  * @ORM\Table(name="promotion", indexes={@ORM\Index(name="IDX_C11D7DD1A76ED395", columns={"user_id"})})
  * @ORM\Entity
+ * @ORM\HasLifecycleCallbacks
  */
 class Promotion
 {
@@ -28,13 +31,6 @@ class Promotion
     private $name;
 
     /**
-     * @var string|null
-     *
-     * @ORM\Column(name="code", type="string", length=255, nullable=true)
-     */
-    private $code;
-
-    /**
      * @var \DateTime|null
      *
      * @ORM\Column(name="start_at", type="datetime", nullable=true)
@@ -49,6 +45,27 @@ class Promotion
     private $endAt;
 
     /**
+     * @var float|null
+     *
+     * @ORM\Column(name="min_price", type="float", precision=10, scale=0, nullable=true)
+     */
+    private $minPrice;
+
+    /**
+     * @var float|null
+     *
+     * @ORM\Column(name="max_price", type="float", precision=10, scale=0, nullable=true)
+     */
+    private $maxPrice;
+
+    /**
+     * @var float|null
+     *
+     * @ORM\Column(name="reduc_percentage", type="float", precision=10, scale=0, nullable=true)
+     */
+    private $reducPercentage;
+
+    /**
      * @var \DateTime|null
      *
      * @ORM\Column(name="created_at", type="datetime", nullable=true)
@@ -56,7 +73,7 @@ class Promotion
     private $createdAt;
 
     /**
-     * @var \Users
+     * @var Users
      *
      * @ORM\ManyToOne(targetEntity="Users")
      * @ORM\JoinColumns({
@@ -65,5 +82,141 @@ class Promotion
      */
     private $user;
 
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
 
+    /**
+     * @return string|null
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string|null $name
+     */
+    public function setName(string $name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return \DateTime|null
+     */
+    public function getStartAt(): \DateTime
+    {
+        return $this->startAt;
+    }
+
+    /**
+     * @param \DateTime|null $startAt
+     */
+    public function setStartAt(\DateTime $startAt)
+    {
+        $this->startAt = $startAt;
+    }
+
+    /**
+     * @return \DateTime|null
+     */
+    public function getEndAt(): \DateTime
+    {
+        return $this->endAt;
+    }
+
+    /**
+     * @param \DateTime|null $endAt
+     */
+    public function setEndAt(\DateTime $endAt)
+    {
+        $this->endAt = $endAt;
+    }
+
+    /**
+     * @return float|null
+     */
+    public function getMinPrice(): float
+    {
+        return $this->minPrice;
+    }
+
+    /**
+     * @param float|null $minPrice
+     */
+    public function setMinPrice(float $minPrice)
+    {
+        $this->minPrice = $minPrice;
+    }
+
+    /**
+     * @return float|null
+     */
+    public function getMaxPrice(): float
+    {
+        return $this->maxPrice;
+    }
+
+    /**
+     * @param float|null $maxPrice
+     */
+    public function setMaxPrice(float $maxPrice)
+    {
+        $this->maxPrice = $maxPrice;
+    }
+
+    /**
+     * @return float|null
+     */
+    public function getReducPercentage(): float
+    {
+        return $this->reducPercentage;
+    }
+
+    /**
+     * @param float|null $reducPercentage
+     */
+    public function setReducPercentage(float $reducPercentage)
+    {
+        $this->reducPercentage = $reducPercentage;
+    }
+
+    /**
+     * @return \DateTime|null
+     */
+    public function getCreatedAt(): \DateTime
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * Gets triggered only on insert
+
+     * @ORM\PrePersist
+     */
+    public function onPrePersist()
+    {
+        $this->createdAt = new \DateTime("now");
+    }
+
+    /**
+     * @return Users
+     */
+    public function getUser(): Users
+    {
+        return $this->user;
+    }
+
+    /**
+     * @param Users $user
+     */
+    public function setUser(Users $user)
+    {
+        $this->user = $user;
+    }
 }
