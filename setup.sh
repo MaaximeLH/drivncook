9d152c4802ce127eb964b9b50422c6b81b205126
MaaximeLH <lehenaff.maxime@yahoo.Fr>
2020-07-10 00:49:01 +0200

    Update setup script

--- setup.sh
+++ setup.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
 composer install
+mkdir Documents
+mkdir Documents/Invoices
 mkdir public/dist/qrcode
 chmod 777 public/dist/qrcode
+chmod 777 Documents/Invoices
 
 echo 'Setup finished'
