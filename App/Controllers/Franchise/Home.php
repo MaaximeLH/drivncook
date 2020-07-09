a378828b1334de820e933ddd6e53fc29ddd5d7af
MaaximeLH <lehenaff.maxime@yahoo.Fr>
2020-07-09 16:46:02 +0200

    Système d'horaires d'ouverture pour les franchisés

--- App/Controllers/Franchise/Home.php
+++ App/Controllers/Franchise/Home.php
@@ -2,6 +2,7 @@
 
 namespace App\Controllers\Franchise;
 
+use App\Entity\Openings;
 use App\Entity\Users;
 use App\Entity\Warehouse;
 use Core\Controller;
@@ -37,7 +38,102 @@ class Home extends Controller
             }
         }
 
-        return View::render('Franchise/index', ['user' => $user, 'page' => 'index', 'warehousesLocates' => json_encode($warehousesLocates)]);
+        $openingsRepository = $em->getRepository(Openings::class);
+        $openings = $openingsRepository->findOneByUser($user);
+        if(is_null($openings)) {
+
+            $date = new \DateTime();
+            $open = $date->setTime(0, 0);
+            $date = new \DateTime();
+            $close = $date->setTime(23, 59);
+
+            $opening = new Openings();
+            $opening->setUser($user);
+
+            $opening->setMondayOpen($open);
+            $opening->setTuesdayOpen($open);
+            $opening->setWedOpen($open);
+            $opening->setThursdayOpen($open);
+            $opening->setFridayOpen($open);
+            $opening->setSatOpen($open);
+            $opening->setSunOpen($open);
+
+            $opening->setMondayClose($close);
+            $opening->setTuesdayClose($close);
+            $opening->setWedClose($close);
+            $opening->setThursdayClose($close);
+            $opening->setFridayClose($close);
+            $opening->setSatClose($close);
+            $opening->setSunClose($close);
+
+            $opening->setGlobalOpen(1);
+
+            $em->persist($opening);
+            $em->flush();
+
+            $openings = $openingsRepository->findOneByUser($user);
+        }
+
+        $update = false;
+        if(Request::isPost()) {
+            $params = Request::getAllParams();
+
+            foreach ($params as $key => $value) {
+                if($key != "global_open") {
+
+                    $time = new \DateTime();
+                    $tmp = explode(" ", str_replace(":", " ", $value));
+                    $params[$key] = $time->setTime($tmp[0], $tmp[1]);
+
+                }
+            }
+
+            if($params['monday_open'] < $params['monday_close']) {
+                $openings->setMondayOpen($params['monday_open']);
+                $openings->setMondayClose($params['monday_close']);
+            }
+
+            if($params['tuesday_open'] < $params['tuesday_close']) {
+                $openings->setTuesdayOpen($params['tuesday_open']);
+                $openings->setTuesdayClose($params['tuesday_close']);
+            }
+
+            if($params['wed_open'] < $params['wed_close']) {
+                $openings->setWedOpen($params['wed_open']);
+                $openings->setWedClose($params['wed_close']);
+            }
+
+            if($params['thursday_open'] < $params['thursday_close']) {
+                $openings->setThursdayOpen($params['thursday_open']);
+                $openings->setThursdayClose($params['thursday_close']);
+            }
+
+            if($params['friday_open'] < $params['friday_close']) {
+                $openings->setFridayOpen($params['friday_open']);
+                $openings->setFridayClose($params['friday_close']);
+            }
+
+            if($params['sat_open'] < $params['sat_close']) {
+                $openings->setSatOpen($params['sat_open']);
+                $openings->setSatClose($params['sat_close']);
+            }
+
+            if($params['sun_open'] < $params['sun_close']) {
+                $openings->setSunOpen($params['sun_open']);
+                $openings->setSunClose($params['sun_close']);
+            }
+
+            if(isset($params['global_open'])) {
+                $openings->setGlobalOpen(0);
+            } else {
+                $openings->setGlobalOpen(1);
+            }
+
+            $update = true;
+            $em->flush();
+        }
+
+        return View::render('Franchise/index', ['user' => $user, 'page' => 'index', 'warehousesLocates' => json_encode($warehousesLocates), 'openings' => $openings, 'update' => $update]);
     }
 
     public function loginAction() {
